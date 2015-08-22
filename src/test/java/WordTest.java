03ce77eda01b1bc0fd54b4b8ec1b67ffa342fe89
Diana Holland <holla@bikefarm.org>
2015-08-21 22:30:41 -0700

    Tests pass.

--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -3,4 +3,10 @@ import static org.junit.Assert.*;
 
 public class WordTest {
 
+  @Test
+  public void word_instantiatesCorrectly_true() {
+    Word myWord = new Word("stampede");
+    assertEquals(true, myWord instanceof Word);
+  }
+
 }
