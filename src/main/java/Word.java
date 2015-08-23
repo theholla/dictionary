7bd4549a6a3543713ee0dfca83baaa7ef677e5aa
Diana Holland <holla@bikefarm.org>
2015-08-22 20:55:05 -0700

    New Word Class and one Passing Test

---
+++ src/main/java/Word.java
@@ -0,0 +1,20 @@
+public class Word {
+  private String mName;
+
+  public Word(String name) {
+    mName = name;
+  }
+
+  public String addDefinition() {
+    something
+  }
+
+  public String getDefinition() {
+    something
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+}
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -1,4 +1,4 @@
-/*import org.junit.*;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class WordTest {
@@ -12,10 +12,15 @@ public class WordTest {
   @Test
   public void word_instantiatesWithName_givesName() {
     Word myWord = new Word("stampede");
-    assertEquals("stampede", myWord.getWord());
+    assertEquals("stampede", myWord.getName()); //Name is just a string
   }
 
-
+  @Test
+  public void test_addAndGetDefintion() {
+    Word myWord = new Word("stampede");
+    Definition myDefinition = new Definition("sudden panicked run");
+    myWord.addDefinition(myDefinition); // myDefinition is an instance of class Definition
+    assertEquals(myDefinition, myWord.getDefinition());
+  }
 
 }
-*/
